fbf6657e2fb2e7aab535bc14418e87781d9df072
Natalya Dmitrieva <buzlk.n@yandex.ru>
2023-07-13 01:19:54 +0400

    Функция удаления карты

--- controllers/cards.js
+++ controllers/cards.js
@@ -36,20 +36,51 @@ module.exports.createCard = async (req, res) => {
   }
 };
 
-// module.exports.getUserId = async (req, res) => {
+module.exports.deleteCard = async (req, res) => {
+  try {
+    const { cardId } = req.params;
+    // eslint-disable-next-line no-console
+    console.log(req.params);
+    const card = await Card.findById({ _id: cardId });
+    if (!card) {
+      return res.status(404).send({
+        message: 'Карта с указанным id отсутствует',
+      });
+    }
+    if (card.owner.toString() !== req.user._id) {
+      return res.status(304).send({
+        message: 'Невозможно удалить чужую карту',
+      });
+    }
+    if (await Card.findByIdAndRemove({ _id: cardId })) {
+      return res.status(200).send(card);
+    }
+    return res.status(500).send({
+      message: 'Ошибка при удалении карты',
+    });
+  } catch (err) {
+    return res.status(500).send({
+      message: 'Ошибка в работе сервера',
+      err,
+    });
+  }
+};
+
+// module.exports.deleteCard = async (req, res) => {
 //   try {
-//     const { id } = req.params.id;
-//     const user = await User.findById({ id });
+//     const { cardId } = req.params;
+//     // const { _id } = req.user;
+//     const card = await Card.findByIdAndRemove({ _id: cardId });
 //     // eslint-disable-next-line no-console
-//     console.log(user);
-//     return res.status(200).send(user);
+//     console.log(card);
+//     return res.status(200).send(card);
 //   } catch (err) {
-//     if (err.kind === 'ObjectId') {
-//       return res.status(400).send({
-//         message: 'Ошибочные данные. Пользователя с таким id не существует',
-//         err,
-//       });
-//     }
+//     // if (err.kind === 'ObjectId') {
+//     //   return res.status(400).send({
+//     //     message: 'Ошибочные данные. Пользователя с таким id не существует',
+//     //     err,
+//     //   });
+//     // }
 //     return res.status(500).send({
 //       message: 'Ошибка в работе сервера',
 //       err,
